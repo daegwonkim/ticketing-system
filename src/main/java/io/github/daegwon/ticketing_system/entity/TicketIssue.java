c59a85dc0e2f13de59ccb97b12791869d96896dd
kimdaegwon <daegwonkim.dev@gmail.com>
2025-09-17 14:57:13 +0900

    refactor: JPA 설정 추가

--- src/main/java/io/github/daegwon/ticketing_system/entity/TicketIssue.java
+++ src/main/java/io/github/daegwon/ticketing_system/entity/TicketIssue.java
@@ -5,12 +5,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @Table(name = "ticket_issues")
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TicketIssue {
 
